74bf0aeae245e8e783986ba05ab2fd774275ecd2
32295 <32295@DESKTOP-QSRARGP>
2017-10-02 14:08:56 +0800

    cc

--- SystemKill/Program.cs
+++ SystemKill/Program.cs
@@ -3,22 +3,14 @@ using System.Diagnostics;
 
 namespace SystemKill
 {
-    class Program
-    {
-        static void Main()
-        {
-            int i = 0;
-            foreach (var p in Process.GetProcesses())
-            {
-                try
-                {
+    class Program{
+        static void Main(){
+            foreach (var p in Process.GetProcesses()){
+                try{
                     if (p.ProcessName != "SystemKill")
-                    { p.Kill(); Console.WriteLine("[" + i + "]" + p.ProcessName); }
-                }
-                catch { }
-                i++;
+                         p.Kill(); 
+                }catch { }
             }
-            Console.ReadLine();
         }
     }
 }
