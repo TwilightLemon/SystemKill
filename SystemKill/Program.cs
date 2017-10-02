2b24bc1af41e3723df895fc35c0ce4e93643b23f
32295 <32295@DESKTOP-QSRARGP>
2017-10-02 13:29:38 +0800

    添加项目文件。

---
+++ SystemKill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SystemKill
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            foreach (var p in Process.GetProcesses())
+            {
+                try
+                {
+                    if (p.ProcessName != "SystemKill")
+                    { p.Kill(); Console.WriteLine("[" + i + "]" + p.ProcessName); }
+                }
+                catch { }
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
